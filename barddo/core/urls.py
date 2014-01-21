b96ca2fccbd5e2953d056af61265c4cc045bfc33
israel.crisanto <devnull@localhost>
2014-01-21 01:32:43 -0200

    Preparando a casca da implementacao e cadastro de colecoes

--- barddo/core/urls.py
+++ barddo/core/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import patterns, url
 from .views import index
+from .views import collection_list, collection_create
 
 urlpatterns = patterns('',
 
     url(r'^$',
         index,
         name='core.index'),
+
+    url(r'^collections/create$',
+        collection_create,
+        name='collections.create'),
+
+    url(r'^collections$',
+        collection_list,
+        name='collections.list'),
 )
--- barddo/core/views.py
+++ barddo/core/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+###
+### Collection related views
+###
+
+
+def collection_list(request):
+    return render(request, "index.html")
+
+
+def collection_create(request):
+    return render(request, "collection_create.html")
