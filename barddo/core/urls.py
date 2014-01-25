fa5401f85b8f3bebca9ae35383bbbd7f4fe289c1
bruno.ortiz <devnull@localhost>
2014-01-25 14:28:47 -0200

    Criando class based views para não replicar código que carrega o contexto do usuario

--- barddo/core/urls.py
+++ barddo/core/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import patterns, url
 from .views import index
-from .views import collection_list, collection_create
+from .views import collection_list, create_collection
 
-urlpatterns = patterns('',
-
+urlpatterns = patterns(
+    '',
     url(r'^$',
         index,
         name='core.index'),
 
     url(r'^collections/create$',
-        collection_create,
+        create_collection,
         name='collections.create'),
 
     url(r'^collections$',
