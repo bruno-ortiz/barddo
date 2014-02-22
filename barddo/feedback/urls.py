82c0b7b3f044720379ce7e090a0c11e943929207
josealmada <devnull@localhost>
2014-02-21 23:34:54 -0300

    Removendo URL nao utilizada

--- barddo/feedback/urls.py
+++ barddo/feedback/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import patterns, url
-from feedback.views import FeedbackView
+from django.conf.urls import patterns
 
 __author__ = 'jose'
 
 urlpatterns = patterns(
     '',
-    url(r'^feedback$', FeedbackView.as_view(), name='feedback'),
 
 )
