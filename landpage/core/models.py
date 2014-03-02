a277e5bcee4a6769a907343bc7fbd91dc5ad7c34
israel.crisanto <devnull@localhost>
2014-03-02 17:26:12 -0300

    Ajustes de textos para traducao

--- landpage/core/models.py
+++ landpage/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class UserContact(models.Model):
+    """
+    A simple entity to hold user contact messages
+    """
+    name = models.CharField("Name", max_length=255)
+    email = models.EmailField("Email")
+    message = models.TextField("Message")
