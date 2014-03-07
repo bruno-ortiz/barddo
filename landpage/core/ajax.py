a3866ea24f84ca5aaae7aa2c6f9be0da7e7a470d
israel.crisanto <devnull@localhost>
2014-03-06 23:11:32 -0300

    Ajustes na pagina de landing

---
+++ landpage/core/ajax.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from dajax.core import Dajax
+from dajaxice.utils import deserialize_form
+from dajaxice.decorators import dajaxice_register
+
+from .forms import UserContactForm
+
+
+@dajaxice_register
+def register_a_collection(request, form):
+    """
+    Called asynchronously to validate/save a new collection.
+    Right now we only do a simple model validation.
+    """
+    ajax = Dajax()
+    form = UserContactForm(deserialize_form(form))
+
+    if form.is_valid():
+        form.save()
+        ajax.script('callback_feedback_sent()')
+    else:
+        ajax.script('callback_feedback_error()')
+        ajax.remove_css_class("#feedback-form div.form-group", "has-error")
+
+        for field, errors in form.errors.items():
+            ajax.script("error_tooltip('#id_%s', '%s');" % (field, "<br />".join(errors)))
+            ajax.script('$("#id_%s").closest("div.form-group").addClass("has-error")' % field)
+
+    return ajax.json()
