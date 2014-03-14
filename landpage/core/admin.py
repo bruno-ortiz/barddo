d03349b5e2484b56b641c2ee556f794bb67e0880
israel.crisanto <devnull@localhost>
2014-03-14 00:37:12 -0300

    ajustes gerais

--- landpage/core/admin.py
+++ landpage/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserContact
+
+
+class UserContactAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserContact, UserContactAdmin)
