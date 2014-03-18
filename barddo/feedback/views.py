f018bce17f2f1c3df6df3eea5f944a69cdf88179
israel.crisanto <devnull@localhost>
2014-03-17 23:44:11 -0300

    Organizando tela de feedback, agora ela carrega os dados do usuario, de estiver logado.

--- barddo/feedback/views.py
+++ barddo/feedback/views.py
@@ -6,7 +6,17 @@ from shards.decorators import register_shard
 
 @register_shard(name=u"feedback")
 class FeedbackModalView(TemplateResponseMixin, View):
+    """
+    Just create the feedback form modal, if user is logged in, then we use it's information,
+    otherwise, we let the user provide email and name
+    """
     template_name = 'feedback-modal.html'
 
-    def post(self, *args, **kwargs):
-        return super(FeedbackModalView, self).render_to_response({})
+    def post(self, request, *args, **kwargs):
+        context = {}
+
+        if request.user.is_authenticated():
+            context["username"] = request.user.get_full_name()
+            context["email"] = request.user.email
+
+        return super(FeedbackModalView, self).render_to_response(context)
