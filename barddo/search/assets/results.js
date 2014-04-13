33ddff1d27179c433e3f945d02809100f983d02c
josealmada <devnull@localhost>
2014-04-12 21:13:51 -0300

    Criando javascript próprio para o JQueryContainer

---
+++ barddo/search/assets/results.js
@@ -0,0 +1,57 @@
+jQuery(function () {
+
+    function applyIsotope() {
+
+    }
+
+    var jQuerycontainer = jQuery('.results');
+
+
+    jQuerycontainer.imagesLoaded(function () {
+        jQuerycontainer.show();
+
+        jQuerycontainer.isotope({
+            itemSelector: '.item',
+            filter: '.result-collection',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+    });
+
+
+    var jQueryoptionSets = jQuery('#options .option-set'),
+        jQueryoptionLinks = jQueryoptionSets.find('a');
+
+    jQueryoptionLinks.click(function () {
+        var jQuerythis = jQuery(this);
+        // don't proceed if already selected
+        if (jQuerythis.hasClass('selected')) {
+            return false;
+        }
+        var jQueryoptionSet = jQuerythis.parents('.option-set');
+        jQueryoptionSet.find('.selected').removeClass('selected');
+        jQuerythis.addClass('selected');
+
+        // make option object dynamically, i.e. { filter: '.my-filter-class' }
+        var options = {},
+            key = jQueryoptionSet.attr('data-option-key'),
+            value = jQuerythis.attr('data-option-value');
+        // parse 'false' as false boolean
+        value = value === 'false' ? false : value;
+        options[key] = value;
+        if (key === 'layoutMode' && typeof changeLayoutMode === 'function') {
+            // changes in layout modes need extra logic
+            changeLayoutMode(jQuerythis, options)
+        } else {
+            // otherwise, apply new options
+            jQuerycontainer.isotope(options);
+        }
+
+        return false;
+    });
+
+
+});
