f298ee9542f0d43801da8ef3a5ab6db26e24c99e
jose.almada <devnull@localhost>
2014-04-14 17:48:02 -0300

    Commit inicial da mudança no padrão do main_template

--- barddo/barddo/settings/base.py
+++ barddo/barddo/settings/base.py
@@ -155,6 +155,7 @@ TEMPLATE_LOADERS = (
 )
 
 TEMPLATE_DIRS = (
+    path.normpath(path.join(SITE_ROOT, '', 'templates')),
     path.normpath(path.join(SITE_ROOT, 'core', 'templates')),
     path.normpath(path.join(SITE_ROOT, 'feedback', 'templates')),
     path.normpath(path.join(SITE_ROOT, 'shards', 'templates')),
