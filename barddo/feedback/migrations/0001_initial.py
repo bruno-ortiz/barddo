c38fcd0bef6f8836ddd0d065182fca25795e8afa
israel.crisanto <devnull@localhost>
2014-05-19 23:19:47 -0300

    Ajustes

--- barddo/feedback/migrations/0001_initial.py
+++ barddo/feedback/migrations/0001_initial.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
 
 
 class Migration(SchemaMigration):
-
     def forwards(self, orm):
         # Adding model 'Feedback'
         db.create_table(u'feedback_feedback', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=30)),
-            ('address', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
             ('feedback', self.gf('django.db.models.fields.TextField')(max_length=500)),
             ('date', self.gf('django.db.models.fields.DateTimeField')()),
         ))
@@ -27,7 +24,7 @@ class Migration(SchemaMigration):
     models = {
         u'feedback.feedback': {
             'Meta': {'object_name': 'Feedback'},
-            'address': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
             'date': ('django.db.models.fields.DateTimeField', [], {}),
             'feedback': ('django.db.models.fields.TextField', [], {'max_length': '500'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
