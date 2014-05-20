499a8a2e187b40350587a3d80a2c64f20e795b25
israel.crisanto <devnull@localhost>
2014-05-20 01:42:30 -0300

    Logando erros para emails dos administradores

--- barddo/barddo/settings/production.py
+++ barddo/barddo/settings/production.py
@@ -27,13 +27,13 @@ ALLOWED_HOSTS = ['.barddo.com', 'barddo.com', '127.0.0.1:8000']
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
-EMAIL_HOST = environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_HOST = environ.get('EMAIL_HOST', 'email-smtp.us-east-1.amazonaws.com')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
-EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD', 'barddo@01')
+EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD', 'ApndY0q7AeWJZBQSop07FbJVwF2gW8n5PE6IUVt7PYTn')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
-EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', 'barddoteam')
+EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', 'AKIAIEAC5JJOJSQDSQVA')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
 EMAIL_PORT = environ.get('EMAIL_PORT', 587)
