9e803805ac28906ec17ea5ee09ffc7f9d657d5c0
bruno.ortiz <devnull@localhost>
2014-06-01 15:27:51 -0300

    Apagando loaddata

--- barddo/payments/migrations/0001_initial.py
+++ barddo/payments/migrations/0001_initial.py
@@ -1,16 +1,80 @@
 # -*- coding: utf-8 -*-
-from south.v2 import DataMigration
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
 
 
-class Migration(DataMigration):
+class Migration(SchemaMigration):
+
     def forwards(self, orm):
-        from django.core.management import call_command
+        # Adding model 'PaymentMethod'
+        db.create_table(u'payments_paymentmethod', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=20)),
+        ))
+        db.send_create_signal(u'payments', ['PaymentMethod'])
 
-        call_command("loaddata", "payment_method.json")
-        call_command("loaddata", "purchase_status.json")
+        # Adding model 'Payment'
+        db.create_table(u'payments_payment', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('code', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('creation_date', self.gf('django.db.models.fields.DateField')(db_index=True)),
+            ('settled_date', self.gf('django.db.models.fields.DateField')(null=True, db_index=True)),
+            ('method', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['payments.PaymentMethod'])),
+        ))
+        db.send_create_signal(u'payments', ['Payment'])
+
+        # Adding model 'PurchaseStatus'
+        db.create_table(u'payments_purchasestatus', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=20)),
+        ))
+        db.send_create_signal(u'payments', ['PurchaseStatus'])
+
+        # Adding model 'Purchase'
+        db.create_table(u'payments_purchase', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('date', self.gf('django.db.models.fields.DateField')(db_index=True)),
+            ('total', self.gf('django.db.models.fields.DecimalField')(max_digits=10, decimal_places=2)),
+            ('payment', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['payments.Payment'], unique=True, null=True)),
+            ('buyer', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['accounts.BarddoUser'])),
+            ('status', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['payments.PurchaseStatus'])),
+        ))
+        db.send_create_signal(u'payments', ['Purchase'])
+
+        # Adding model 'Item'
+        db.create_table(u'payments_item', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('purchase', self.gf('django.db.models.fields.related.ForeignKey')(related_name='items', to=orm['payments.Purchase'])),
+            ('work', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Work'])),
+            ('price', self.gf('django.db.models.fields.DecimalField')(max_digits=6, decimal_places=2)),
+        ))
+        db.send_create_signal(u'payments', ['Item'])
+
+        # Adding unique constraint on 'Item', fields ['purchase', 'work']
+        db.create_unique(u'payments_item', ['purchase_id', 'work_id'])
+
 
     def backwards(self, orm):
-        "Write your backwards methods here."
+        # Removing unique constraint on 'Item', fields ['purchase', 'work']
+        db.delete_unique(u'payments_item', ['purchase_id', 'work_id'])
+
+        # Deleting model 'PaymentMethod'
+        db.delete_table(u'payments_paymentmethod')
+
+        # Deleting model 'Payment'
+        db.delete_table(u'payments_payment')
+
+        # Deleting model 'PurchaseStatus'
+        db.delete_table(u'payments_purchasestatus')
+
+        # Deleting model 'Purchase'
+        db.delete_table(u'payments_purchase')
+
+        # Deleting model 'Item'
+        db.delete_table(u'payments_item')
+
 
     models = {
         u'accounts.barddouser': {
@@ -18,8 +82,7 @@ class Migration(DataMigration):
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [],
-                       {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
@@ -27,8 +90,7 @@ class Migration(DataMigration):
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [],
-                                 {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
         u'auth.group': {
@@ -38,16 +100,14 @@ class Migration(DataMigration):
             'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
         u'auth.permission': {
-            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)",
-                     'object_name': 'Permission'},
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
         u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType',
-                     'db_table': "'django_content_type'"},
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
@@ -89,7 +149,7 @@ class Migration(DataMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '6', 'decimal_places': '2'}),
             'purchase': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'items'", 'to': u"orm['payments.Purchase']"}),
-            'work': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['core.Work']", 'unique': 'True'})
+            'work': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Work']"})
         },
         u'payments.payment': {
             'Meta': {'object_name': 'Payment'},
@@ -121,4 +181,3 @@ class Migration(DataMigration):
     }
 
     complete_apps = ['payments']
-    symmetrical = True
