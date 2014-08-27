313675eac17c63d4302a11029859d8e402b4be6a
bruno.ortiz <devnull@localhost>
2014-08-26 23:51:41 -0300

    Corrigindo mask de conta e agencia e mensagens

--- barddo/payments/forms.py
+++ barddo/payments/forms.py
@@ -1,9 +1,27 @@
+import re
+
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
+from django.utils.translation import ugettext as _
 
 from payments.models import BankAccount
 
 
 class BankAccountForm(ModelForm):
+    PATTERN = re.compile(r'^\d+\-?\d+$')
+
     class Meta:
         model = BankAccount
         exclude = ('user',)
+
+    def clean_account(self):
+        account = self.cleaned_data.get('account')
+        if not self.PATTERN.match(account):
+            raise ValidationError(_("Account must contains only numbers or '-' separators and finish with a number."))
+        return account
+
+    def clean_agency(self):
+        agency = self.cleaned_data.get('agency')
+        if not self.PATTERN.match(agency):
+            raise ValidationError(_("Agency must contains only numbers or '-' separators and finish with a number."))
+        return agency
