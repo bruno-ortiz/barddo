efa7d7b99968283f6cae0c337561aad94f3fcf33
israel.crisanto <devnull@localhost>
2014-10-24 18:15:45 -0200

    Removendo a landpage

--- landpage/core/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from .models import UserContact
-
-
-class UserContactForm(ModelForm):
-    class Meta:
-        model = UserContact
-        fields = ['name', 'email', 'message']
